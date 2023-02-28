90a211c8be6028200a970e3a91b5aa184070e40a
thongstylish <tranthongspkt@gmail.com>
2023-03-01 01:53:41 +0700

    frist commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Qmobile</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
