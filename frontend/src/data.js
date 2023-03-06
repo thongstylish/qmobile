2ff95c26dcf0f93141a11f57cc140bf591b65f75
thongstylish <tranthongspkt@gmail.com>
2023-03-07 01:13:36 +0700

    add to cart

---
+++ frontend/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+  products: [
+    {
+      name: 'Iphone XS',
+      slug: 'iphone-xs',
+      category: 'Iphone',
+      image: '/images/1.jpg', // 679px x 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'XS',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Iphone XS 64GB',
+    },
+    {
+      name: 'Iphone XR',
+      slug: 'iphone-xr',
+      category: 'Iphone',
+      image: '/images/2.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'XR',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Iphone XR 64GB',
+    },
+    {
+      name: 'Iphone X',
+      slug: 'iphone-x',
+      category: 'Iphone',
+      image: '/images/3.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'X',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Iphone X 64GB',
+    },
+  ],
+};
+export default data;
